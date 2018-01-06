d3a8b43391e33415f2808ae680e9bb8a257d4afb
André Abadesso <andre@lab21k.com.br>
2018-01-06 16:58:14 -0200

    fix: fixed typo

--- topic_alert.py
+++ topic_alert.py
@@ -81,25 +81,3 @@ rows = cur.fetchall()
 topics = map(lambda x: create_dict(x), rows)
 
 print topics
-
-"""
-try:
-    for row in rows:
-        topic_id = row[0]
-        date = "now()"
-        num_pages = row[3]
-        count_read = row[4]
-
-        cur.execute("""
-        INSERT INTO topic_daily
-        (topic_id, date, num_pages, count_read) VALUES
-        ({0}, {1}, {2}, {3})
-        """.format(topic_id, date, num_pages, count_read))
-except Exception as e:
-    print "Erroed"
-    print e
-    cur.execute("ROLLBACK")
-else:
-    cur.execute("COMMIT")
-"""
-
